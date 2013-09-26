d312088b0e045aafbe816597f82dedd926691a20
Shane <shbburke@gmail.com>
2013-09-26 15:54:48 -0700

    Finally finished this shit, fucking IE...

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length:{minimum:1, maximum:21}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence:true, uniqueness:{ case_sensitive: false }, format:{with:VALID_EMAIL_REGEX}
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, presence:true
   has_secure_password
 end
 
